2ebf833aae5dfabb9dcbd3b85b4a1c550a91e522
lingeorge88 <lingeorge04@gmail.com>
2023-04-04 21:16:26 -0700

    added models and relations between models

---
+++ models/User.js
@@ -0,0 +1,70 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+
+// create fields/columns for User model
+User.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      job_id: {
+        type: DataTypes.INTEGER,
+        references: {
+        model: 'job',
+        key: 'id',
+      }
+    },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [5]
+        }
+    },
+},
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+              // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+          },
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    },
+  );
+
+  module.exports = User;
