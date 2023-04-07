d1637f79e7452d1b62eb645ef879b659a5893b76
Goobber33 <kcparks1234@gmail.com>
2023-04-06 20:09:40 -0700

    Added get route to get users by id

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,6 +17,21 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
--- server.js
+++ server.js
@@ -5,22 +5,27 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 // Initialize the app
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars with helpers
+
 const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Set up middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up session middleware
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -34,9 +39,11 @@ app.use(
 );
 
 // Set up routes
+
 app.use('/', routes);
 
 // Start the server
+
 (async () => {
   try {
     await sequelize.sync({ force: false });
