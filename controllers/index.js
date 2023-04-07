1fa0bde30b58adb58132064adbfcd937636113f7
Goobber33 <kcparks1234@gmail.com>
2023-04-06 19:16:59 -0700

    Changed home-routes to index.js and added the router to get the login.handlebars page

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+// Home page route
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+// User and job routes
+const userRoutes = require('./api/userRoutes');
+const jobRoutes = require('./api/jobRoutes');
+router.use('/users', userRoutes);
+router.use('/jobs', jobRoutes);
+
+module.exports = router;
