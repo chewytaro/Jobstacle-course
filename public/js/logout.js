4d20d2f898890da8fa519516aafea4782d39e639
Goobber33 <kcparks1234@gmail.com>
2023-04-10 20:07:53 -0700

    fixed logout button to log the user out and return to the login page.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
 const logout = async () => {
+  console.log('Logout function called');
+
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    console.log('Logout successful');
+    // Wait for a short time before redirecting to ensure session is destroyed
+    setTimeout(() => {
+      document.location.replace('/');
+    }, 500);
   } else {
+    console.log('Logout failed');
     alert(response.statusText);
   }
 };
-
-document.querySelector('#logout').addEventListener('click', logout);
