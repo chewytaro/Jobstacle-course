b0bc5903a661cfffae0cf2dcf53ceed45500ee59
Meljska-Fawn <meli.explores@gmail.com>
2023-04-11 19:38:08 -0700

    fixed create job form handler

--- public/js/myApplications.js
+++ public/js/myApplications.js
@@ -1,40 +1,24 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#job-name').value.trim();
+    const title = document.querySelector('#job-name').value.trim();
     const salary = document.querySelector('#salary').value.trim();
     const description = document.querySelector('#job-desc').value.trim();
     const status = document.querySelector('#status').value.trim();
   
-    if (name && salary && description && staus) {
-      const response = await fetch(`/api/jobRoutes`, {
+    if (title && salary && description && status) {
+      const response = await fetch(`/api/jobs`, {
         method: 'POST',
-        body: JSON.stringify({ name, salary, description, status }),
+        body: JSON.stringify({ title, salary, description, status }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
   
       if (response.ok) {
-        document.location.replace('/myApplications');
+        document.location.replace('/api/jobs');
       } else {
-        alert('Failed to create project');
-      }
-    }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/jobRoutes/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/myApplications');
-      } else {
-        alert('Failed to delete project');
+        alert('Failed to create job');
       }
     }
   };
@@ -43,7 +27,4 @@ const newFormHandler = async (event) => {
     .querySelector('.new-project-form')
     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
   
