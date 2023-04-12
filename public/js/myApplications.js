95ea8d2340a4f70d25547e35a8a6e3adb04c2669
lingeorge88 <lingeorge04@gmail.com>
2023-04-11 21:26:01 -0700

    added missing myapp.js file

---
+++ public/js/myApplications.js
@@ -0,0 +1,25 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#job-name').value.trim();
+  const salary = document.querySelector('#salary').value.trim();
+  const description = document.querySelector('#job-desc').value.trim();
+  const status = document.querySelector('#status').value.trim();
+  const tag = document.querySelector('#job-tag').value.trim();
+  if (title && salary && description && status && tag ) {
+    const response = await fetch(`/api/jobs`, {
+      method: 'POST',
+      body: JSON.stringify({ title, salary, description, status, tag }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/api/jobs');
+    } else {
+      alert('Failed to create job');
+    }
+  }
+};
+document
+  .querySelector('.new-project-form')
+  .addEventListener('submit', newFormHandler);
