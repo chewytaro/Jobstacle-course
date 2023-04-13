d8d339b38f2e9c5ea73f78c87412f8e19ad75a88
Goobber33 <kcparks1234@gmail.com>
2023-04-13 08:46:04 -0700

    Added changes

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,26 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const logoutButton = document.querySelector("#logout");
+document.addEventListener('DOMContentLoaded', () => {
+  const logoutButton = document.querySelector('#logout');
+  const backButton = document.querySelector('#back');
+
   if (logoutButton) {
-    logoutButton.addEventListener("click", logout);
+    logoutButton.addEventListener('click', async () => {
+      const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+
+      if (response.ok) {
+        setTimeout(() => {
+          document.location.replace('/login');
+        }, 500);
+      } else {
+        alert(response.statusText);
+      }
+    });
+  }
+
+  if (backButton) {
+    backButton.addEventListener('click', () => {
+      document.location.replace('/welcome');
+    });
   }
 });
-
-const logout = async () => {
-  console.log('Logout function called');
-
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (response.ok) {
-    console.log('Logout successful');
-    // Wait for a short time before redirecting to ensure session is destroyed
-    setTimeout(() => {
-      document.location.replace('/login');
-    }, 500);
-  } else {
-    console.log('Logout failed');
-    alert(response.statusText);
-  }
-};
