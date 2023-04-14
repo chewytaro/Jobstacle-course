9d7f51c47fdd3c6505ea3178fe24aa5dab8f2a7e
Goobber33 <kcparks1234@gmail.com>
2023-04-13 19:33:18 -0700

    Added changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
   try {
