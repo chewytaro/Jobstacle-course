383b6a982cb7b5124c069b0ffe64e15cee0434a0
Meljska-Fawn <meli.explores@gmail.com>
2023-04-17 13:51:44 -0700

    tried to display reviews

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -2,20 +2,24 @@ const { User } = require('../models');
 
 const userData = [
   {
-    username: "johnsmith",
-    password: "password123",
+    "user_id": 1,
+    "username": "johnsmith",
+    "password": "password123",
   },
   {
-    username: "janedoe",
-    password: "mypassword",
+    "user_id": 2,
+    "username": "janedoe",
+    "password": "mypassword",
   },
   {
-    username: "bobross",
-    password: "happytrees",
+    "user_id": 3,
+    "username": "bobross",
+    "password": "happytrees",
   },
   {
-    username: "sarahlee",
-    password: "pastrychef",
+    "user_id": 4,
+    "username": "sarahlee",
+    "password": "pastrychef",
   }
 ];
 
